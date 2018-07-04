6bdf9d49405fc2aff476c8e8178d2c2ac06d32d6
Paveen A <40364733+AngryBeam@users.noreply.github.com>
2018-07-04 18:40:17 +0700

    Merged into single file

--- public_html/js/drawsomething.js
+++ public_html/js/drawsomething.js
@@ -1,9 +1,33 @@
-
+const elements = {
+    loadBody: document.querySelector('#loadBody')
+};
 
 window.onload = function (e) {
+    renderLoader(elements.loadBody);
     liff.init(function (data) {
         initializeApp(data);
     });
+    
+};
+
+const elementStrings = {
+    loader: 'loader'
+};
+
+const renderLoader = parent => {
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="images/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if (loader) loader.parentElement.removeChild(loader);
 };
 
 function initializeApp(data) {
