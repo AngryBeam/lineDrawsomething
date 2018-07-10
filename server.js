58b1cba052dda8fb633b4e0005e171c99ede6bfc
Paveen Asawasuebsakul <40364733+AngryBeam@users.noreply.github.com>
2018-07-10 11:08:33 +0700

    Save drawing data

--- server.js
+++ server.js
@@ -203,7 +203,7 @@ app.get('/users/me/:id', (req, res) => {    //Using a middle ware for authentica
     }
   });
 
-  app.get('/users/save', (req, res) => {    //Using a middle ware for authenticate
+  app.post('/users/save', (req, res) => {    //Using a middle ware for authenticate
     console.log(JSON.stringify(req.body, null ,2));
   });
 
